513322b63091a2396dca3b507195843ba556dfbf
PedroLudovic144 <pedroludovic5@gmail.com>
2024-08-26 18:50:43 -0300

    New commit

--- pw2-etec-project/pw2-etec-project/src/main/java/br/com/etechoracio/pw2_etec_project/Pw2EtecProjectApplication.java
+++ pw2-etec-project/pw2-etec-project/src/main/java/br/com/etechoracio/pw2_etec_project/Pw2EtecProjectApplication.java
@@ -1,13 +1,13 @@
-package br.com.etechoracio.pw2_etec_projeto;
+package br.com.etechoracio.pw2_etec_project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Pw2EtecProjetoApplication {
+public class Pw2EtecProjectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Pw2EtecProjetoApplication.class, args);
+		SpringApplication.run(Pw2EtecProjectApplication.class, args);
 	}
 
 }
